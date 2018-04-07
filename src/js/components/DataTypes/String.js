c09369f4c693bafce146c0455bbfe8a4323b95e1
Nicolae Rusan <nicolaerusan@Nicolaes-MBP.fios-router.home>
2018-04-07 13:52:00 -0400

    Adding clickable string values when URL

--- src/js/components/DataTypes/String.js
+++ src/js/components/DataTypes/String.js
@@ -35,7 +35,24 @@ export default class extends React.Component {
         this.setState(this.state)
     }
 
+    isValidURL(str) => {
+      var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+        '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+        '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+        '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+        '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+        '(\#[-a-z\d_]*)?$','i'); // fragment locater
+      if(!pattern.test(str)) {
+        alert("Please enter a valid URL.");
+        return false;
+      } else {
+        return true;
+      }
+    }
+
     render() {
+
+
         const type_name = "string"
         const { collapsed } = this.state
         const { props } = this
@@ -55,6 +72,8 @@ export default class extends React.Component {
                 )
             }
         }
+
+        let renderStringValue = this.isValidUrl(value) ? <a target='_blank' href={value}>"{value}"</a> : "{value}";
 
         return (
             <div {...Theme(theme, "string")}>
